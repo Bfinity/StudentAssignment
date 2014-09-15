9af654edbd7e2d0b9c1b6c89f24bc15151abeddf
Brandy Foster <brandy.foster@detroitlabs.com>
2014-09-15 14:21:46 -0400

    created student class with variables.

---
+++ src/com/sample/studentassignment/Student.java
@@ -0,0 +1,47 @@
+package com.sample.studentassignment;
+
+/**
+ * Created by BFineRocks on 9/15/14.
+ */
+
+/**
+ * This class creates the template for a new student object.
+ */
+public class Student {
+    int identifcationNumber;
+    int numberOfCredits;
+    int numberOfPoints;
+    double gradePointAverage;
+
+    public void setIdentifcationNumber(int identifcationNumber) {
+        this.identifcationNumber = identifcationNumber;
+    }
+
+    public void setNumberOfCredits(int numberOfCredits) {
+        this.numberOfCredits = numberOfCredits;
+    }
+
+    public void setNumberOfPoints(int numberOfPoints) {
+        this.numberOfPoints = numberOfPoints;
+    }
+
+    public void calculateGradePointAverage() {
+        this.gradePointAverage = numberOfPoints / numberOfCredits;
+    }
+
+    public int getIdentifcationNumber() {
+        return identifcationNumber;
+    }
+
+    public int getNumberOfCredits() {
+        return numberOfCredits;
+    }
+
+    public int getNumberOfPoints() {
+        return numberOfPoints;
+    }
+
+    public double getGradePointAverage() {
+        return gradePointAverage;
+    }
+}
